e7288b435995fba5345499556d51fd309d9bd94d
mahitha481 <mahitha481@gmail.com>
2026-03-23 10:06:23 +0530

    initial commit

---
+++ arithematic.py
@@ -0,0 +1,15 @@
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def multi(a,b):
+    return a*b
+def divi(a,b):
+    return a/b
+def moduler(a,b):
+    return a%b
+print("addition:",add(12,3))
+print("substraction:",sub(12,3))
+print("multiplication:",multi(12,3))
+print("division:",divi(12,3))
+print("moduler:",moduler(12,3))
