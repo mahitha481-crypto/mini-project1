0739c4c8c76d7c530548c35beb87071634d962b3
mahitha481 <mahitha481@gmail.com>
2026-03-23 12:57:50 +0530

    initial commit

---
+++ student result.py
@@ -0,0 +1,9 @@
+def student_result(name,marks):
+    if marks>=50:
+        print(name,"pass")
+    else:
+        print(name,"fail")
+student_result("mahi",95)
+student_result("navya",70)
+student_result("pravallika",50)
+student_result("anusha",45)
